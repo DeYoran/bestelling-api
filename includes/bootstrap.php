c28ce9044d2424dac41ea24ecadb832c67633de9
Yoran <Yoran.engelberts@gmail.com>
2018-02-22 15:18:42 +0100

    vlidator and config template added

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -2,5 +2,11 @@
 
 require_once("includes/config.php");
 require_once("includes/doctrine.php");
+
+if(CONFIG_VERSION !== $templateversion){
+    die('invalid config file');
+}
+
 require_once("includes/oauth2.php");
 require_once('controller/Controller.php');
+require_once('includes/Validator.php');
---
+++ includes/config.template.php
@@ -0,0 +1,14 @@
+<?php
+$templateversion = "0.1.a";
+/* CONFIG FILE SHOULD LOOK LIKE THIS
+define("CONFIG_VERSION","0.1.a");
+
+//paths
+define("BASE_URL","http://example.com");
+
+//database
+define("DBHOST","localhost");
+define("DBUSER","user");
+define("DBPASS","password");
+define("DBNAME","databasename");
+*/
